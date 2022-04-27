407758be2ae487366fe20c3af4f896787364b65d
sherikarh1 <93055798+sherikarh1@users.noreply.github.com>
2022-04-27 00:41:59 -0400

    Hash Map pduplicate words

---
+++ src/collection/hashmap/Leet_duplicateWords.java
@@ -0,0 +1,39 @@
+// Find and display number of duplicate occurences of word in String.
+
+package collection.hashmap;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Leet_duplicateWords {
+    public static void findDuplicate(String words){
+        String[] arry = words.split(" ");
+        HashMap<String,Integer>map = new HashMap<>();
+
+        for(int i = 0;i<arry.length;i++){
+            if(map.containsKey(arry[i])){
+                int temp = map.get(arry[i]);
+                map.put(arry[i],temp+1);
+
+            }else {
+                map.put(arry[i],1);
+            }
+        }
+        for(Map.Entry<String,Integer>entry:map.entrySet()){
+            if(entry.getValue()>1){
+                String temp1 = entry.getKey();
+                System.out.println("The word" +temp1 + "occures" +entry.getKey());
+
+            }
+        }
+
+
+
+    }
+
+    public static void main(String[] args) {
+        String words = "I am ama java learner";
+        findDuplicate(words);
+    }
+}
