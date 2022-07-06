2ee84469215db02fe0191c29ca05d09c7f51060e
sherikarh1 <93055798+sherikarh1@users.noreply.github.com>
2022-07-06 13:12:29 -0400

    Reverse Link List

---
+++ src/collection/list/ReversedListDemo.java
@@ -0,0 +1,73 @@
+
+// Refrence : https://www.youtube.com/watch?v=jY-EUKXYT20
+
+package collection.list;
+
+public class ReversedListDemo {
+    private Node head;
+
+    public void reversed(){
+        Node next = null;
+        Node previous = null;
+        Node current = head;
+
+
+        while(current!=null){
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+
+        }
+        if (previous != null)
+            head = previous;
+
+
+    }
+
+    public void addNode(int data){
+        Node node = new Node(data);
+        if(head == null){
+            head = node;
+            return;
+        }
+        Node temp = head;
+        while (temp.next!=null){
+            temp=temp.next;
+        }
+        temp.next=node;
+
+    }
+
+    public void showLinkedList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.getData());
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        ReversedListDemo r = new ReversedListDemo();
+
+        r.addNode(10);
+        r.addNode(20);
+        r.addNode(30);
+        r.addNode(40);
+        r.showLinkedList();
+        r.reversed();
+        r.showLinkedList();
+
+    }
+}
+
+class Node {
+    private int data;
+    Node next;
+    Node(int data) {
+        this.data = data;
+    }
+    public int getData() {
+        return data;
+    }
+}
